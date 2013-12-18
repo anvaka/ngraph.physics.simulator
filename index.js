577e25aac71ae5c873aa1e4087d66fa66ad7d692
anvaka <anvaka@gmail.com>
2013-12-17 22:15:24 -0800

    Added euler integrator

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Manages a simulation of physical forces acting on bodies.
+ * To create a custom force simulator register forces of the system
+ * via addForce() method, choos appropriate integrator and register
+ * bodies.
+ */
+module.exports = function (forceIntegrator) {
+};
