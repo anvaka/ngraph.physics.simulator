e6436abe8961fa481b5a621676c9fedbc558362b
anvaka <anvaka@gmail.com>
2013-12-18 01:07:58 -0800

    Added public interface

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 /**
- * Manages a simulation of physical forces acting on bodies.
- * To create a custom force simulator register forces of the system
- * via addForce() method, choos appropriate integrator and register
- * bodies.
+ * Manages a simulation of physical forces acting on bodies and springs.
  */
-module.exports = function (forceIntegrator) {
+module.exports = function () {
+  var integrate = require('./lib/eulerIntegrator');
+
+  var bodies = [], // Bodies in this simulation.
+      springs = []; // Springs in this simulation.
+
+  return {
+    /**
+     * Performs one step of force simulation.
+     * @param {Number} timeStep - integration step
+     *
+     * @returns {Number} total distance traveled by bodies/total # of bodies
+     */
+    step: function (timeStep) {
+      accumulateForces();
+      return integrate(bodies, timeStep);
+    },
+
+    addBody: function (body) {
+      if (!body) {
+        throw new Error('Body is required');
+      }
+      bodies.push(body);
+
+      return body;
+    }
+  };
+
+  function accumulateForces() {
+
+  }
 };
