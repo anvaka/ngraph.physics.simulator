499ddeb12a48fe1fef286493c162bca15a68b5c8
anvaka <anvaka@gmail.com>
2013-12-31 01:37:16 -0800

    100% coverage for dragForce

--- test/dragForce.js
+++ test/dragForce.js
@@ -7,7 +7,7 @@ test('reduces force value', function (t) {
   body.force.x = 1; body.force.y = 1;
   body.velocity.x = 1; body.velocity.y = 1;
 
-  var dragForce = createDragForce({ coeff: 0.1 });
+  var dragForce = createDragForce({ dragCoeff: 0.1 });
   dragForce.update(body);
 
   t.ok(body.force.x < 1 && body.force.y < 1, 'Force value is reduced');
@@ -16,16 +16,19 @@ test('reduces force value', function (t) {
 
 test('Initialized with default value', function (t) {
   var dragForce = createDragForce();
-  var defaults = dragForce.options();
-  t.ok(defaults && typeof defaults.coeff === 'number', 'Default value is present');
+  var dragCoeff = dragForce.dragCoeff();
+  t.ok(typeof dragCoeff === 'number', 'Default value is present');
 
   t.end();
 });
 
 test('Can update default value', function (t) {
   var dragForce = createDragForce();
-  var options = dragForce.options({ coeff: 0.0 }).options();
+  var returnedForce = dragForce.dragCoeff(0.0);
 
-  t.ok(options.coeff === 0.0, 'Default value is updated');
+  t.ok(dragForce === returnedForce, 'Allows chaining');
+
+  var dragCoeff = dragForce.dragCoeff();
+  t.ok(dragCoeff === 0.0, 'Default value is updated');
   t.end();
 });
