da01a23f6383cd085cf8bf0526bb90f0a1269bfb
anvaka <anvaka@gmail.com>
2013-12-31 00:43:14 -0800

    Added settings to the simulator

--- index.js
+++ index.js
@@ -3,12 +3,50 @@
  */
 module.exports = physicsSimulator;
 
-function physicsSimulator() {
+function physicsSimulator(settings) {
   var Spring = require('./lib/spring');
   var createQuadTree = require('ngraph.quadtreebh');
   var createDragForce = require('./lib/dragForce');
   var createSpringForce = require('./lib/springForce');
   var integrate = require('./lib/eulerIntegrator');
+  var merge = require('ngraph.merge');
+
+  settings = merge(settings, {
+      /**
+       * Ideal length for links (springs in physical model).
+       */
+      springLength: 80,
+
+      /**
+       * Hook's law coefficient. 1 - solid spring.
+       */
+      springCoeff: 0.0002,
+
+      /**
+       * Coulomb's law coefficient. It's used to repel nodes thus should be negative
+       * if you make it positive nodes start attract each other :).
+       */
+      gravity: -1.2,
+
+      /**
+       * Theta coeffiecient from Barnes Hut simulation. Ranged between (0, 1).
+       * The closer it's to 1 the more nodes algorithm will have to go through.
+       * Setting it to one makes Barnes Hut simulation no different from
+       * brute-force forces calculation (each node is considered).
+       */
+      theta: 0.8,
+
+      /**
+       * Drag force coefficient. Used to slow down system, thus should be less than 1.
+       * The closer it is to 0 the less tight system will be.
+       */
+      dragCoeff: 0.02,
+
+      /**
+       * Default time step (dt) for forces integration
+       */
+      timeStep : 20
+  });
 
   var bodies = [], // Bodies in this simulation.
       springs = [], // Springs in this simulation.
@@ -27,16 +65,15 @@ function physicsSimulator() {
 
     /**
      * Performs one step of force simulation.
-     * @param {Number} timeStep - integration step
      *
      * @returns {Number} Total movement of the system. Calculated as:
      *   (total distance traveled by bodies)^2/(total # of bodies)
      */
-    step: function (timeStep) {
+    step: function () {
       // I'm reluctant to check timeStep here, since this method is going to be
       // super hot, I don't want to add more complexity to it
       accumulateForces();
-      return integrate(bodies, timeStep);
+      return integrate(bodies, settings.timeStep);
     },
 
     /**
